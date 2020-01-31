1653e78799af76f49b6745a40dfd487f4045bb67
Paderina Olga <JoIL.Paderina@gmail.com>
2020-01-31 13:37:12 +0300

    Набросок интерфейса пользователя.

---
+++ Tests/TestConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.Mail.MailMessage msg = new MailMessage ("sender@server.ru", "recipient@server.ru");
+            msg.Subject = "Тема сообщения";
+            msg.Body = "Тело сообщения";
+            msg.IsBodyHtml = false;
+            msg.Attachments.Add(new Attachment("c:\\file.exe"));
+
+            System.Net.Mail.SmtpClient client = new SmtpClient ("smtp@yandex.ru", 25);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("UserNane", "Password");
+
+            client.Send(msg);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tests/TestWPF/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestWPF
+{
+      public partial class App
+    {
+        private void OnApplicationStartup(object sender, StartupEventArgs e)
+        {
+
+        }
+    }
+}
