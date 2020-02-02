a847d9db926aa2d3a9fdc5443e004ace5d9a143a
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-02 21:50:29 +0300

    Примеры работы с панелями компоновки.

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MailSender
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
