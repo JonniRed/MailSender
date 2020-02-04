2188b8532d7df7855d7f71b670a002c89e01836d
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-04 18:17:41 +0300

    TestData

---
+++ MailSender.lib/Data/TestData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.lib.Entities;
+
+namespace MailSender.lib.Data
+{
+    public class TestData
+    {
+        public static List<Server> Servers { get; } = new List<Server>
+        {
+            new Server {Name = "Яндекс", Adress="smtp.yandex.ru", Port = 587, Login="UserLogin", Password = "UserPassord" }
+        }
+        public static List<Sender> Senders { get; }
+        }
+    }
+}
--- MailSender.lib/Entities/Server.cs
+++ MailSender.lib/Entities/Server.cs
@@ -9,7 +9,7 @@ namespace MailSender.lib.Entities
         public string Name { get; set; }
         public string Adress { get; set; }
         public int Port { get; set; }
-        public bool UseSsl { get; set; }
+        public bool UseSsl { get; set; } = true;
 
         public string Login { get; set;}
         public string Password { get; set; }
