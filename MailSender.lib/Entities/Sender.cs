cdc333d5c40de8d246e185091fa95cef3d107c7d
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-04 18:01:33 +0300

    Создание библиотеки

---
+++ MailSender.lib/Entities/Sender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Entities
+{
+    public class Sender
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+}
