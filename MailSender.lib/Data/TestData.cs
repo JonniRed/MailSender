d7d7c6f1a5c1fa22455e67dbe58540c3ebcf542e
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-05 11:06:54 +0300

    TestData_фиксация

--- MailSender.lib/Data/TestData.cs
+++ MailSender.lib/Data/TestData.cs
@@ -9,9 +9,17 @@ namespace MailSender.lib.Data
     {
         public static List<Server> Servers { get; } = new List<Server>
         {
-            new Server {Name = "Яндекс", Adress="smtp.yandex.ru", Port = 587, Login="UserLogin", Password = "UserPassord" }
-        }
-        public static List<Sender> Senders { get; }
-        }
+            new Server {Name = "Яндекс", Adress="smtp.yandex.ru", Port = 587, Login="UserLogin", Password = "UserPassord"},
+            new Server {Name = "Mail.ru", Adress="smtp.yandex.ru", Port = 587, Login="UserLogin", Password = "UserPassord"},
+            new Server {Name = "Gmail", Adress="smtp.gmail.com", Port = 587, Login="UserLogin", Password = "UserPassord" }
+
+        };
+        public static List<Sender> Senders { get; } = new List<Sender>
+        {
+            new Sender {Name = "Иванов", Adress = "ivanov@server.ru"},
+            new Sender {Name = "Петров", Adress = "petrov@server.ru"},
+             new Sender {Name = "Сидоров", Adress = "sidorov@server.ru"}
+        };
+                
     }
 }
