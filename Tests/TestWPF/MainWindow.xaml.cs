82fe5b6a6630e9f93ecdab56ebdd3af9eb9a7a8b
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-07 14:36:24 +0300

    Создание MVVM в MailSender.

--- Tests/TestWPF/MainWindow.xaml.cs
+++ Tests/TestWPF/MainWindow.xaml.cs
@@ -11,41 +11,5 @@ namespace TestWPF
     {
         public MainWindow() => InitializeComponent();
         
-        private void OnSendButtonClick(object sender, RoutedEventArgs e)
-        {
-            var message_subject = $"Текстовое сообщение от {DateTime.Now}";
-            var message_body = $"Тело сообщения - {DateTime.Now}";
-
-            const string from = "joil.paderina@yandex.ru";
-            const string to = "joil.paderina@gmail.com";
-
-            try
-            {
-                using (var message = new MailMessage(from, to))
-                {
-                    message.Subject = message_subject;
-                    message.Body = message_body;
-
-
-                    const string host = "smtp.yandex.ru";
-                    const int port = 25;
-                    using (var client = new SmtpClient(host, port))
-                    {
-                        client.EnableSsl = true;
-                        var user_name = UserNameEdit.Text;
-                        System.Security.SecureString secure_password = PasswordEdit.SecurePassword;
-
-                        client.Credentials = new NetworkCredential(user_name, secure_password);
-                        client.Send(message);
-
-                        MessageBox.Show("Письмо успешно отправлено!", "Y", MessageBoxButton.OK, MessageBoxImage.Information);
-                    }
-                }
-            }
-            catch (Exception error)
-            {
-                MessageBox.Show(error.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-        }
     }
 }
