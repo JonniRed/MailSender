a30a2aee3858b9e68944a600ccfaf3a0faeeb419
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-08 19:04:18 +0300

    Добавлено ViewModelLocator

---
+++ MailSender/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+
+namespace MailSender.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+        }
+    }
+}
