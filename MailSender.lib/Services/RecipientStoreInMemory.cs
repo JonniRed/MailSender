52da2ffc6b1f27ce06e81b7699adf4767936e89e
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-13 10:42:23 +0300

    Хранилище данных (RecipientStoreInMemory) - описание логики работы

--- MailSender.lib/Services/RecipientStoreInMemory.cs
+++ MailSender.lib/Services/RecipientStoreInMemory.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using MailSender.lib.Entities;
 using MailSender.lib.Data;
 using MailSender.lib.Services.Interfaces;
@@ -10,7 +10,32 @@ namespace MailSender.lib.Services
     public class RecipientStoreInMemory : IRecipientStore
     {
         public IEnumerable<Recipient> GetAll() => TestData.Recipient;
-        public void Edit(int Id, Recipient recipient) { }
-        public void SaveChanges() { }
+        public Recipient GetById(int Id) => TestData.Recipient.FirstOrDefault(r => r.Id == Id);
+        public int Create(Recipient Recipient)
+        {
+            if (TestData.Recipient.Contains(Recipient)) return Recipient.Id;
+            Recipient.Id = TestData.Recipient.Count == 0 ? 1 : TestData.Recipient.Max(r => r.Id) + 1;
+            TestData.Recipient.Add(Recipient);
+            return Recipient.Id;
+        }
+        public void Edit(int Id, Recipient recipient)
+        {
+            var db_recipient = GetById(Id);
+            if (db_recipient is null) return;
+
+            db_recipient.Name = recipient.Name;
+            db_recipient.Adress = recipient.Adress;
+        }
+        public Recipient Remove (int Id)
+        {
+            var db_recipient = GetById(Id);
+            if(db_recipient != null)
+            TestData.Recipient.Remove(db_recipient);
+            return db_recipient;
+        }
+        public void SaveChanges()
+        {
+            System.Diagnostics.Debug.WriteLine("Сохранение изменений в хранилище");
+        }
     }
 }
