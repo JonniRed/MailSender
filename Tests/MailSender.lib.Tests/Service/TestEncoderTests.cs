a28bbe5ee79e91ebad356a8901bccec702443875
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-14 11:54:57 +0300

    Создание первого модульного теста

---
+++ Tests/MailSender.lib.Tests/Service/TestEncoderTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.lib.Service;
+
+namespace MailSender.lib.Tests.Service
+{
+    [TestClass]
+    public class TestEncoderTests
+    {
+        [TestMethod]
+        public void Encode_ABC_to_BCD_with_key_1()
+        {
+            const string str = "ABC";
+            const int key = 1;
+            const string expected_str = "BCD";
+
+            var actual_str = TextEncoder.Encode(str, key);
+
+            Assert.AreEqual(expected_str, actual_str);
+        }
+    }
+}
