74b6ced493cd10fb6995735e9a2a56e62d07f958
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-14 11:05:28 +0300

    Валидация с помощью IDataErrorInfo

--- MailSender.lib/Entities/Recipient.cs
+++ MailSender.lib/Entities/Recipient.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using MailSender.lib.Entities.Base;
 
 namespace MailSender.lib.Entities
 {
-    public class Recipient : PersonEntity
+    public class Recipient : PersonEntity, IDataErrorInfo
     {
         public override string Name
         {
@@ -14,6 +15,22 @@ namespace MailSender.lib.Entities
             {
                 if (value == "Иванов123") throw new ArgumentException("Иванов123 нам не подходит");
                 base.Name = value;
+            }
+        }
+        string IDataErrorInfo.Error => null;
+        string IDataErrorInfo.this[string PropertyName]
+        {
+            get
+            {
+                switch(PropertyName)
+                {
+                    default: return null;
+                    case nameof(Name): var name = Name;
+                        if (name is null) return "Пустая ссылка на имя";
+                        if (name.Length < 2) return "Имя должно быть длиннее двух символов";
+                        if (name.Length > 20) return "Длина имени не должна быть больше 20 символов";
+                        return null;
+                }
             }
         }
     }
