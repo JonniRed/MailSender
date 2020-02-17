80089f59a67598c022b1e22f1b4d7bd1cf910788
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-17 22:46:42 +0300

    Пул потоков, пример использования.

--- Tests/TestConsole/TestConsole/Program.cs
+++ Tests/TestConsole/TestConsole/Program.cs
@@ -5,12 +5,10 @@ namespace TestConsole
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-
+            //ThreadPoolTests.Start();
+            Console.WriteLine("Приложение должно быть закрыто");
         }
-
-    }
-        
+    }   
 }
