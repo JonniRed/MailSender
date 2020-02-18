445a3933daf465006428fb53d4c7636f2eb6f111
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-18 11:34:05 +0300

    Изменение в классе отправки почты - отделили класс Дебаг.

---
+++ MailSender.lib/DebugMailSender.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using MailSender.lib.Entities;
+
+namespace MailSender.lib
+{
+    public class DebugMailSender
+    {
+        private readonly Server _Server;
+
+
+        public DebugMailSender(Server Server) => _Server = Server;
+      
+
+        public void Send(Mail mail, Sender from, Recipient to)
+        {
+            Debug.WriteLine("Отправка почты от {0} к {1} через {2}:{3}[4]", from.Adress, to.Adress, _Server.Adress,
+               _Server.Port, _Server.UseSsl ? "SSL" : "no SSL", mail.Subject, mail.Body);
+        }
+     }
+}
