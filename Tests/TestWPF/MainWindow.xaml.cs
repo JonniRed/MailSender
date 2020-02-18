fa8ab6d64fce5b0ee01f97ecf196ef5598595a3c
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-18 14:48:17 +0300

    Пример использования асинхронного потока (всё равно понимания не прибавилось, у меня очень тормозной ноут)

--- Tests/TestWPF/MainWindow.xaml.cs
+++ Tests/TestWPF/MainWindow.xaml.cs
@@ -2,7 +2,8 @@
 using System.Windows;
 using System.Net;
 using System.Net.Mail;
-
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestWPF
 {
@@ -10,6 +11,28 @@ namespace TestWPF
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
-        
+
+        private async void OnStartButtonClick(object sender, RoutedEventArgs e)
+        {
+            const string message = "Hello me";
+            var result = await GetMessageLengthAsync(message);
+            Result.Text = result.ToString();
+        }
+
+        private void OnCancelButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private Task<int> GetMessageLengthAsync(string Message, int Timeout = 30)
+        {
+            return Task.Run(() => GetMessageLength(Message, Timeout));
+
+        }
+        private int _StartCount;
+        private int GetMessageLength(string Message, int Timeout = 30)
+        {
+            for (var i = 0; i < 100; i++) Thread.Sleep(Timeout);
+            return Message.Length + _StartCount++;
+        }
     }
 }
