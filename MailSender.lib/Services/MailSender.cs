337ab6d500e38cb6bac7c378b165bd4d6eaf561e
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-20 12:13:53 +0300

    Появление синхронного и асинхронного метода отправки писем.

--- MailSender.lib/Services/MailSender.cs
+++ MailSender.lib/Services/MailSender.cs
@@ -4,6 +4,8 @@ using MailSender.lib.Entities;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace MailSender.lib.Services
 {
@@ -38,5 +40,33 @@ namespace MailSender.lib.Services
             foreach (var recipient in To)
                 ThreadPool.QueueUserWorkItem(_ => Send(Message, From, To));
         }
-    }
-}
+
+        public async Task SendAsync(Mail Mail, Sender From, Recipient To)
+        {
+            using (var message = new MailMessage(new MailAddress(From.Adress, From.Name),
+               new MailAddress(To.Adress, To.Name)))
+            {
+                message.Subject = Mail.Subject;
+                message.Body = Mail.Body;
+
+                var login = new NetworkCredential(_Server.Login, _Server.Password);
+                using (var client = new SmtpClient(_Server.Adress, _Server.Port)
+                { EnableSsl = _Server.UseSsl, Credentials = login })
+                    await client.SendMailAsync(message).ConfigureAwait(false);
+            }
+
+        }
+       /* public async Task SendAsync(Mail Message, Sender From, IEnumerable<Recipient> To)
+        {
+            await Task.WhenAll(To.Select(Recipient => SendAsync(Message, From, To))).ConfigureAwait(false);
+        }
+        (Асинхронная параллельная обработка данных)*/
+        public async Task SendAsync(Mail Message, Sender From, IEnumerable<Recipient> To,
+            CancellationToken Cancel = default)
+        {
+            Cancel.ThrowIfCancellationRequested();
+            foreach (var recipient in To) await SendAsync(Message, From, To).ConfigureAwait(false);
+        }
+        ///Асинхронная последовательная обработка данных
+     }
+ }
