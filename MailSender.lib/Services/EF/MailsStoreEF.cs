1c1f4e75edb619226c41c86a4df1fb39f33d0320
Paderina Olga <JoIL.Paderina@gmail.com>
2020-02-26 12:06:49 +0300

    Работа с БД - создание хранилищ писем, данных серверов.

---
+++ MailSender.lib/Services/EF/MailsStoreEF.cs
@@ -0,0 +1,23 @@
+﻿using MailSender.lib.Data.EF;
+using MailSender.lib.Entities;
+using MailSender.lib.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Services.EF
+{
+    public class MailsStoreEF : StoreEF<Mail>, IMailStore
+    {
+        public MailsStoreEF(MailSenderDB db) : base(db) { }
+        public override void Edit(int Id, Mail item) 
+        {
+            var db_item = GetById(Id);
+            if (db_item == null) return;
+
+            db_item.Subject = item.Subject;
+            db_item.Body = item.Body;
+        }
+    }
+    
+}
---
+++ MailSender.lib/Services/EF/ServerStoreEF.cs
@@ -0,0 +1,27 @@
+﻿using MailSender.lib.Data.EF;
+using MailSender.lib.Entities;
+using MailSender.lib.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Services.EF
+{
+   public class ServerStoreEF : StoreEF<Server>//, IServerStore
+    {
+        public ServerStoreEF(MailSenderDB db) : base(db) { }
+
+        public override void Edit(int Id, Server item)
+        {
+            var db_item = GetById(Id);
+            if (db_item is null) return;
+
+            db_item.Name = item.Name;
+            db_item.Adress = item.Adress;
+            db_item.Port = item.Port;
+            db_item.UseSsl = item.UseSsl;
+            db_item.Login = item.Login;
+            db_item.Password = item.Password;
+        }
+    }
+}
